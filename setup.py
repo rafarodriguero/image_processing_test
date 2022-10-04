6a17b436814676d9198cd2d14a00260fbd3b6e07
Souza <r.souza@accenture.com>
2022-10-04 16:29:13 -0300

    Atualizado a branch com os códigos

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="package_name",
+    version="0.0.1",
+    author="Rafael Souza",
+    author_email="rafaelrodriguero@msn.com",
+    description="Image Processing Package using skimage",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rafarodriguero/image_processing_test",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
